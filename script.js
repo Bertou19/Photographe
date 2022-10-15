f52ed3f87fe988c3d9dbd2fd7ac6a9e71c577f7b
Bertille De Almeida <a_bertille@msn.com>
2022-10-15 16:45:19 +0200

    Correction indentation et dernière vérification du site et fonctionnalités

--- script.js
+++ script.js
@@ -53,38 +53,3 @@ function displayPics() {
 window.onload = displayPics;
 
 
-/*FORMULAIRE DE CONTACT*/
-
-//Pour envoyer le message du formulaire 
-
-
-var form = document.getElementById("my-form");
-
-async function handleSubmit(event) {
-  event.preventDefault();
-  var status = document.getElementById("my-form-status");
-  var data = new FormData(event.target);
-  fetch(event.target.action, {
-    method: form.method,
-    body: data,
-    headers: {
-      'Accept': 'application/json'
-    }
-  }).then(response => {
-    if (response.ok) {
-      status.innerHTML = "Merci pour votre message! A trés bientôt !";
-      form.reset()
-    } else {
-      response.json().then(data => {
-        if (Object.hasOwn(data, 'errors')) {
-          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-        } else {
-          status.innerHTML = "Oops! Il y a eu un problème lors de l'envoi du formulaire, veuillez réessayer. Merci."
-        }
-      })
-    }
-  }).catch(error => {
-    status.innerHTML = "Oops! Il y a eu un problème lors de l'envoi du formulaire, veuillez réessayer. Merci."
-  });
-}
-form.addEventListener("submit", handleSubmit)
